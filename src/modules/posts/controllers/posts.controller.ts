a6efe462ff9ea2d403594ca87df6420e0f99235d
Vishwa_Dinupriya <vishwad@dharmahomesuites.com>
2022-08-07 17:30:08 +0530

    implmented error handling to the controller

--- src/modules/posts/controllers/posts.controller.ts
+++ src/modules/posts/controllers/posts.controller.ts
@@ -9,45 +9,73 @@ export class PostsController {
 
     @Get()
     async findAll() {
-        // get all posts in the db
-        return await this.postService.findAllPosts();
+        try {
+            // get all posts in the db
+            return await this.postService.findAllPosts();
+        } catch (err) {
+            throw err;
+        }
+
     }
 
     @Post()
     async create(@Body() post: PostDto, @Request() req): Promise<PostEntity> {
-        // create a new post and return the newly created post
-        return await this.postService.createPost(post);
+
+        try {
+            // create a new post and return the newly created post
+            return await this.postService.createPost(post);
+        } catch (err) {
+            throw err;
+        }
+
     }
 
 
     @Put('/like/:id')
     async likePost(@Param('id') id: number): Promise<PostEntity> {
-        const  incrementResult = await this.postService.likePost(id);
 
-        // return the increment Result
-        return incrementResult;
+        try {
+            const incrementResult = await this.postService.likePost(id);
+
+            // return the increment Result
+            return incrementResult;
+        } catch (err) {
+            throw err;
+        }
+
     }
 
     @Put('/dislike/:id')
     async dislikePost(@Param('id') id: number): Promise<PostEntity> {
-        const  incrementResult = await this.postService.dislikePost(id);
+        const incrementResult = await this.postService.dislikePost(id);
 
-        // return the increment Result
-        return incrementResult;
+        try {
+            // return the increment Result
+            return incrementResult;
+        } catch (err) {
+            throw err;
+        }
+
     }
 
     @Delete(':id')
     async remove(@Param('id') id: number, @Request() req) {
-        // delete the post with this id
-        const deleted = await this.postService.deletePost(id);
 
-        // if the number of row affected is zero, 
-        // then the post doesn't exist in our db
-        if (deleted === 0) {
-            throw new NotFoundException('This Post doesn\'t exist');
+        try {
+            // delete the post with this id
+            const deleted = await this.postService.deletePost(id);
+
+            // if the number of row affected is zero, 
+            // then the post doesn't exist in our db
+            if (deleted === 0) {
+                throw new NotFoundException('This Post doesn\'t exist');
+            }
+
+            // return success message
+            return deleted;
+        } catch (err) {
+            throw err;
         }
 
-        // return success message
-        return deleted;
     }
 }
